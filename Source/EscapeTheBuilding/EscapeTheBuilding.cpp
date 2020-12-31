2cab524845dc9c04d24a415a99c96f89a49d897e
StefEmp <stephane.emptage@hotmail.co.uk>
2020-12-31 18:12:30 +0000

    Initial commit

---
+++ Source/EscapeTheBuilding/EscapeTheBuilding.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EscapeTheBuilding.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EscapeTheBuilding, "EscapeTheBuilding" );
---
+++ Source/EscapeTheBuilding/EscapeTheBuildingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeTheBuildingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPETHEBUILDING_API AEscapeTheBuildingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
